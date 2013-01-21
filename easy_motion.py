tednaleid/sublime-EasyMotion

commit bafb2268a5b33f4194b2a24f4c3da36ae00c381d

fix issue #12 - cursor appearance not updated after jump (vintage mode)

easy_motion.py
@@ -196,7 +196,10 @@ class EasyMotionCommand(sublime_plugin.WindowCommand):
 
     def jump_to_winning_selection(self):
         if self.winning_selection is not None:
-            view_sel = self.active_view.sel()
-            view_sel.clear()
-            view_sel.add(self.winning_selection)
-            self.active_view.show(self.winning_selection)
+            self.active_view.run_command('jump_to_winning_selection', {'new_sel': self.winning_selection})
+
+class JumpToWinningSelection(sublime_plugin.TextCommand):
+    def run(self, edit, new_sel):
+        sel = self.view.sel()
+        sel.clear()
+        sel.add(long(new_sel))

commit c5e99350851bbd7ee46446a86f5a38123d198e56

fix jump selection broken by commit bafb2268a5

easy_motion.py
@@ -151,7 +151,7 @@ class EasyMotionCommand(sublime_plugin.WindowCommand):
             if self.select_text:
                 for current_selection in self.active_view.sel():
                     if winning_region.begin() < current_selection.begin():
-                        return sublime.Region(winning_region.begin(), current_selection.end())
+                        return sublime.Region(current_selection.end(), winning_region.begin())
                     else:
                         return sublime.Region(current_selection.begin(), winning_region.end())
             else:
@@ -196,10 +196,16 @@ class EasyMotionCommand(sublime_plugin.WindowCommand):
 
     def jump_to_winning_selection(self):
         if self.winning_selection is not None:
-            self.active_view.run_command('jump_to_winning_selection', {'new_sel': self.winning_selection})
+            if isinstance(self.winning_selection, long):
+                self.active_view.run_command("jump_to_winning_selection", {"a": self.winning_selection})
+            elif isinstance(self.winning_selection, sublime.Region):
+                self.active_view.run_command("jump_to_winning_selection", {"a": self.winning_selection.a, "b": self.winning_selection.b})
 
 class JumpToWinningSelection(sublime_plugin.TextCommand):
-    def run(self, edit, new_sel):
+    def run(self, edit, a, b=None):
         sel = self.view.sel()
         sel.clear()
-        sel.add(long(new_sel))
+        if b is None:
+            sel.add(long(a))
+        else:
+            sel.add(sublime.Region(long(a), long(b)))